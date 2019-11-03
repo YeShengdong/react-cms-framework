58e77f391d98053d61281c14b4f776bfaa85c935
Allen Ye <38345792@qq.com>
2019-11-03 09:20:36 +0800

    init .eslintrc.js file and use airbnb

---
+++ FE/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parser": "babel-eslint",
+    "extends": ["airbnb"],
+    "globals": {},
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/jsx-filename-extension": 0,
+      "react/jsx-one-expression-per-line": 0
+    }
+};
