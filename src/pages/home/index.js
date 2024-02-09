0dac6d7e4cb306cb1664eba789c894300ffc94d7
Allen Ye <38345792@qq.com>
2024-02-09 11:48:26 +0800

    up

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, memo } from 'react';
 
 function HomePage() {
-  return <h1>Home page</h1>;
+  const [count, setCount] = useState(0);
+  console.log('home page');
+  return (
+    <>
+      <h1>Home page</h1>
+      <button type="button" name="button" onClick={() => setCount(count + 1)}>
+        Click me{count}
+      </button>
+      <MemoizedChildOne />
+    </>
+  );
+}
+
+function ChildOne() {
+  console.log('Child One');
+  return (
+    <>
+      <h1>Child One</h1>
+      <ChildTwo />
+    </>
+  );
+}
+
+const MemoizedChildOne = memo(ChildOne);
+
+function ChildTwo() {
+  console.log('Child Two');
+  return <h1>Child One</h1>;
 }
 
 export default HomePage;
